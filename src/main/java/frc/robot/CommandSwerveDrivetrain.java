64aa27c4f1fa9474a89f17e1007c5fa7d01963db
Banks Troutman <btrout.dhrs@gmail.com>
2024-01-13 03:58:10 -0500

    Fix choreo integration, remove RobotContainer

--- src/main/java/frc/robot/CommandSwerveDrivetrain.java
+++ src/main/java/frc/robot/CommandSwerveDrivetrain.java
@@ -10,16 +10,17 @@ import com.ctre.phoenix6.mechanisms.swerve.SwerveDrivetrain;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveDrivetrainConstants;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveModuleConstants;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveRequest;
+import com.ctre.phoenix6.mechanisms.swerve.SwerveRequest.ApplyChassisSpeeds;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 
 import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Notifier;
 import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 
 /**
@@ -28,73 +29,81 @@ import edu.wpi.first.wpilibj2.command.Subsystem;
  * so it can be used in command-based projects easily.
  */
 public class CommandSwerveDrivetrain extends SwerveDrivetrain implements Subsystem {
-    private static final double kSimLoopPeriod = 0.005; // 5 ms
-    private Notifier m_simNotifier = null;
-    private double m_lastSimTime;
+	private static final double kSimLoopPeriod = 0.005; // 5 ms
+	private Notifier m_simNotifier = null;
+	private double m_lastSimTime;
+	private ApplyChassisSpeeds m_autoRequest = new ApplyChassisSpeeds();
 
-    public CommandSwerveDrivetrain(SwerveDrivetrainConstants driveTrainConstants, double OdometryUpdateFrequency,
-            SwerveModuleConstants... modules) {
-        super(driveTrainConstants, OdometryUpdateFrequency, modules);
-        if (Utils.isSimulation()) {
-            startSimThread();
-        }
-    }
+	public CommandSwerveDrivetrain(SwerveDrivetrainConstants driveTrainConstants, double OdometryUpdateFrequency,
+			SwerveModuleConstants... modules) {
+		super(driveTrainConstants, OdometryUpdateFrequency, modules);
+		if (Utils.isSimulation()) {
+			startSimThread();
+		}
+	}
 
-    public CommandSwerveDrivetrain(SwerveDrivetrainConstants driveTrainConstants, SwerveModuleConstants... modules) {
-        super(driveTrainConstants, modules);
-        if (Utils.isSimulation()) {
-            startSimThread();
-        }
-    }
+	public CommandSwerveDrivetrain(SwerveDrivetrainConstants driveTrainConstants, SwerveModuleConstants... modules) {
+		super(driveTrainConstants, modules);
+		if (Utils.isSimulation()) {
+			startSimThread();
+		}
+	}
 
-    public Command applyRequest(Supplier<SwerveRequest> requestSupplier) {
-        return run(() -> this.setControl(requestSupplier.get()));
-    }
+	public Command applyRequest(Supplier<SwerveRequest> requestSupplier) {
+		return run(() -> this.setControl(requestSupplier.get()));
+	}
 
-    private void startSimThread() {
-        m_lastSimTime = Utils.getCurrentTimeSeconds();
+	private void startSimThread() {
+		m_lastSimTime = Utils.getCurrentTimeSeconds();
 
-        /* Run simulation at a faster rate so PID gains behave more reasonably */
-        m_simNotifier = new Notifier(() -> {
-            final double currentTime = Utils.getCurrentTimeSeconds();
-            double deltaTime = currentTime - m_lastSimTime;
-            m_lastSimTime = currentTime;
+		/* Run simulation at a faster rate so PID gains behave more reasonably */
+		m_simNotifier = new Notifier(() -> {
+			final double currentTime = Utils.getCurrentTimeSeconds();
+			double deltaTime = currentTime - m_lastSimTime;
+			m_lastSimTime = currentTime;
 
-            /* use the measured time delta, get battery voltage from WPILib */
-            updateSimState(deltaTime, RobotController.getBatteryVoltage());
-        });
-        m_simNotifier.startPeriodic(kSimLoopPeriod);
-    }
+			/* use the measured time delta, get battery voltage from WPILib */
+			updateSimState(deltaTime, RobotController.getBatteryVoltage());
+		});
+		m_simNotifier.startPeriodic(kSimLoopPeriod);
+	}
 
-    public void logModulePositions() {
-        for (int i = 0; i < Modules.length; i++) {
-            SmartDashboard.putNumber("Module " + i + "/Position",
-                    getModule(i).getDriveMotor().getPosition().getValueAsDouble());
-        }
-    }
+	public void logModulePositions() {
+		for (int i = 0; i < Modules.length; i++) {
+			SmartDashboard.putNumber("Module " + i + "/Position",
+					getModule(i).getDriveMotor().getPosition().getValueAsDouble());
+		}
+	}
 
-    public void setTestMode() {
-        for (int i = 0; i < Modules.length; i++) {
-            getModule(i).getDriveMotor().setNeutralMode(NeutralModeValue.Coast);
-            getModule(i).getSteerMotor().setNeutralMode(NeutralModeValue.Brake);
-        }
-    }
+	public void setTestMode() {
+		for (int i = 0; i < Modules.length; i++) {
+			getModule(i).getDriveMotor().setNeutralMode(NeutralModeValue.Coast);
+			getModule(i).getSteerMotor().setNeutralMode(NeutralModeValue.Brake);
+		}
+	}
 
-    // TODO: make the trajectory chooser at some point
-    public Command choreoSwerveCommand() {
-        ChoreoTrajectory traj = Choreo.getTrajectory("oneMeter");
+	// TODO: make the trajectory chooser at some point
+	public Command choreoSwerveCommand() {
+		ChoreoTrajectory traj = Choreo.getTrajectory("5pc");
 
-        return Choreo.choreoSwerveCommand(
-                traj,
-                this.m_odometry::getEstimatedPosition,
-                new PIDController(Constants.AutoConstants.kPXController, 0.0, 0.0),
-                new PIDController(Constants.AutoConstants.kPXController, 0.0, 0.0),
-                new PIDController(Constants.AutoConstants.kPThetaController, 0.0, 0.0),
-                (ChassisSpeeds speeds) -> this.m_kinematics.toSwerveModuleStates(speeds),
-                () -> {
-                    Optional<DriverStation.Alliance> alliance = DriverStation.getAlliance();
-                    return alliance.isPresent() && alliance.get() == Alliance.Red;
-                },
-                this);
-    }
+		var resetPoseCmd = runOnce(() -> {
+			seedFieldRelative(traj.getInitialPose());
+		});
+
+		var choreoFollowCmd = Choreo.choreoSwerveCommand(
+			traj,
+			()-> getState().Pose,
+			new PIDController(Constants.AutoConstants.kPXController, 0.0, 0.0),
+			new PIDController(Constants.AutoConstants.kPXController, 0.0, 0.0),
+			new PIDController(Constants.AutoConstants.kPThetaController, 0.0, 0.0),
+			(speeds) -> setControl(m_autoRequest.withSpeeds(speeds)),
+			() -> {
+				Optional<DriverStation.Alliance> alliance = DriverStation.getAlliance();
+				return alliance.isPresent() && alliance.get() == Alliance.Red;
+			},
+			this
+		);
+
+		return Commands.sequence(resetPoseCmd, choreoFollowCmd).withName("ChoreoFollower");
+	}
 }
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,25 +4,72 @@
 
 package frc.robot;
 
+import com.ctre.phoenix6.Utils;
+import com.ctre.phoenix6.mechanisms.swerve.SwerveModule.DriveRequestType;
+import com.ctre.phoenix6.mechanisms.swerve.SwerveRequest;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.generated.TunerConstants;
 
 public class Robot extends TimedRobot {
+  private double MaxSpeed = 5; // 6 meters per second desired top speed
+	private double MaxAngularRate = 1.5 * Math.PI; // 3/4 of a rotation per second max angular velocity
+
+	/* Setting up bindings for necessary control of the swerve drive platform */
+	private final CommandXboxController joystick = new CommandXboxController(0); // My joystick
+	public final CommandSwerveDrivetrain drivetrain = TunerConstants.DriveTrain; // My drivetrain
+
+	private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
+		.withDeadband(MaxSpeed * 0.1).withRotationalDeadband(MaxAngularRate * 0.1) // Add a 10% deadband
+		.withDriveRequestType(DriveRequestType.OpenLoopVoltage); // I want field-centric driving in open loop
+	private final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
+	private final SwerveRequest.PointWheelsAt point = new SwerveRequest.PointWheelsAt();
+	private final Telemetry logger = new Telemetry(MaxSpeed);
+
   private Command m_autonomousCommand;
 
-  private RobotContainer m_robotContainer;
+  private void configureBindings() {
+		drivetrain.setDefaultCommand(drivetrain.applyRequest(() -> drive
+			.withVelocityX(-joystick.getLeftY() * MaxSpeed)
+			.withVelocityY(-joystick.getLeftX() * MaxSpeed)
+			.withRotationalRate(-joystick.getRawAxis(2) * MaxAngularRate)
+		));
+
+		joystick.a().whileTrue(drivetrain.applyRequest(() -> brake));
+		joystick.b().whileTrue(drivetrain
+				.applyRequest(
+						() -> point.withModuleDirection(new Rotation2d(-joystick.getLeftY(), -joystick.getLeftX()))));
+
+		// reset the field-centric heading on left bumper press
+		joystick.leftBumper().onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldRelative()));
+
+		if (Utils.isSimulation()) {
+			drivetrain.seedFieldRelative(new Pose2d(new Translation2d(), Rotation2d.fromDegrees(90)));
+		}
+		drivetrain.registerTelemetry(logger::telemeterize);
+	}
+
+  private Command getAutonomousCommand() {
+		return drivetrain.choreoSwerveCommand();
+	}
 
   @Override
   public void robotInit() {
-    m_robotContainer = new RobotContainer();
+    configureBindings();
+
+    // drivetrain.setTestMode();
   }
 
   @Override
   public void robotPeriodic() {
     CommandScheduler.getInstance().run();
-    m_robotContainer.drivetrain.logModulePositions();
-    m_robotContainer.drivetrain.setTestMode();
+    drivetrain.logModulePositions();
   }
 
   @Override
@@ -39,7 +86,7 @@ public class Robot extends TimedRobot {
 
   @Override
   public void autonomousInit() {
-    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+    m_autonomousCommand = getAutonomousCommand();
 
     if (m_autonomousCommand != null) {
       m_autonomousCommand.schedule();
