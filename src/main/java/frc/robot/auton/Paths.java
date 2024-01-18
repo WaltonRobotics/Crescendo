0420b3ba52569668bf7a5463a4e764dabcc7a3b8
nikki <106131029+philosophermurr@users.noreply.github.com>
2024-01-17 21:15:05 -0500

    add grace's path
    i need to get better about actually doing everything i need to do before committing

--- src/main/java/frc/robot/auton/Paths.java
+++ src/main/java/frc/robot/auton/Paths.java
@@ -5,5 +5,6 @@ import com.pathplanner.lib.path.PathPlannerPath;
 public class Paths {
     public static final PathPlannerPath oneMeter = PathPlannerPath.fromChoreoTrajectory("oneMeter");
     public static final PathPlannerPath simpleThing = PathPlannerPath.fromChoreoTrajectory("simpleThing");
+    public static final PathPlannerPath fourPc = PathPlannerPath.fromChoreoTrajectory("4pc");
     public static final PathPlannerPath fivePc = PathPlannerPath.fromChoreoTrajectory("5pc");
 }
