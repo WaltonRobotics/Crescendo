489e038fed4d294fb9677417e6ed2b050aa29cc1
scootdboot <andersonscott100@gmail.com>
2024-02-17 12:05:34 -0500

    removed useless code, added reminders

--- src/main/java/frc/robot/subsystems/shooter/Shooter.java
+++ src/main/java/frc/robot/subsystems/shooter/Shooter.java
@@ -2,22 +2,15 @@ package frc.robot.subsystems.shooter;
 
 import static frc.robot.Constants.ShooterK.kLeftId;
 import static frc.robot.Constants.ShooterK.kRightId;
-import static frc.robot.Constants.ShooterK.FlywheelSimK.*;
 
 import com.ctre.phoenix6.hardware.TalonFX;
 
-import edu.wpi.first.math.system.plant.DCMotor;
-import edu.wpi.first.wpilibj.simulation.FlywheelSim;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Shooter extends SubsystemBase {
     private final TalonFX m_left = new TalonFX(kLeftId);
     private final TalonFX m_right = new TalonFX(kRightId);
-
-    // things for simulation
-    // TODO: check motor number
-    private final FlywheelSim m_flywheelSim = new FlywheelSim(DCMotor.getFalcon500(1), kGearRatio, kMOI);
 
     public Command shoot() {
         return run(() -> {
