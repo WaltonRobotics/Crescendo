19f7e09357add39b147beac75885800fe6db34da
graciexie <xiegrace61@gmail.com>
2024-03-11 21:36:42 -0400

    clear 3 center auton path

--- src/main/java/frc/robot/auton/Paths.java
+++ src/main/java/frc/robot/auton/Paths.java
@@ -6,4 +6,6 @@ public class Paths {
     public static final PathPlannerPath ampSide1 = PathPlannerPath.fromChoreoTrajectory("amp_side.1");
     public static final PathPlannerPath ampSide2 = PathPlannerPath.fromChoreoTrajectory("amp_side.2");
     public static final PathPlannerPath ampSide3 = PathPlannerPath.fromChoreoTrajectory("amp_side.3");
+
+    public static final PathPlannerPath center3 = PathPlannerPath.fromChoreoTrajectory("clear_3_center");
 }
--- src/main/java/frc/robot/auton/Trajectories.java
+++ src/main/java/frc/robot/auton/Trajectories.java
@@ -5,4 +5,6 @@ import com.choreo.lib.ChoreoTrajectory;
 
 public class Trajectories {
 	public static final ChoreoTrajectory ampSide = Choreo.getTrajectory("amp_side");
+	
+	public static final ChoreoTrajectory clear3Center = Choreo.getTrajectory("clear_3_center");
 }
