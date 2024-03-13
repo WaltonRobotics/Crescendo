f084cbdd907ee6ffc05318358a733ac98ab7fbaf
graciexie <xiegrace61@gmail.com>
2024-03-13 19:48:10 -0400

    amp start 3pc

--- src/main/java/frc/robot/auton/Paths.java
+++ src/main/java/frc/robot/auton/Paths.java
@@ -7,7 +7,11 @@ public class Paths {
     public static final PathPlannerPath ampSide2 = PathPlannerPath.fromChoreoTrajectory("amp_side.2");
     public static final PathPlannerPath ampSide3 = PathPlannerPath.fromChoreoTrajectory("amp_side.3");
     public static final PathPlannerPath ampSide4 = PathPlannerPath.fromChoreoTrajectory("amp_side.4");
-    public static final PathPlannerPath ampSideToAmp = PathPlannerPath.fromChoreoTrajectory("amp_side_toAmp");
+    public static final PathPlannerPath ampSideToAmp = PathPlannerPath.fromChoreoTrajectory("amp_3pc_amp");
+
+    public static final PathPlannerPath ampStart1 = PathPlannerPath.fromChoreoTrajectory("amp_start.1");
+    public static final PathPlannerPath ampStart2 = PathPlannerPath.fromChoreoTrajectory("amp_start.2");
+    public static final PathPlannerPath ampStart3 = PathPlannerPath.fromChoreoTrajectory("amp_start.3");
 
     public static final PathPlannerPath center1 = PathPlannerPath.fromChoreoTrajectory("clear_3_center.1");
     public static final PathPlannerPath center2 = PathPlannerPath.fromChoreoTrajectory("clear_3_center.2");
--- src/main/java/frc/robot/auton/Trajectories.java
+++ src/main/java/frc/robot/auton/Trajectories.java
@@ -6,6 +6,8 @@ import com.choreo.lib.ChoreoTrajectory;
 public class Trajectories {
 	public static final ChoreoTrajectory ampSide = Choreo.getTrajectory("amp_side");
 	public static final ChoreoTrajectory ampSideToAmp = Choreo.getTrajectory("amp_3pc_amp");
+
+	public static final ChoreoTrajectory ampStart = Choreo.getTrajectory("amp_start");
 	
 	public static final ChoreoTrajectory clear3Center = Choreo.getTrajectory("clear_3_center");
 }
