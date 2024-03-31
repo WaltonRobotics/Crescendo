d2f4cc5da4e1b4263e97ac1502182282eb05c124
nikki <106131029+philosophermurr@users.noreply.github.com>
2024-03-30 21:53:56 -0400

    loggedtunablenumbers prob still no worky

--- src/main/java/frc/util/logging/LoggedTunableNumber.java
+++ src/main/java/frc/util/logging/LoggedTunableNumber.java
@@ -1,73 +1,47 @@
 package frc.util.logging;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.function.Consumer;
-
-import edu.wpi.first.networktables.NetworkTableEntry;
-import edu.wpi.first.networktables.NetworkTableInstance;
-import frc.robot.Constants.RobotK;
-
-/* reference Mechanical Advantages' LoggedTunableNumber */
-/* this is prob not right, im not entirely sure what im doing help */
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class LoggedTunableNumber {
-    private static final String m_tableKey = "TunableNums";
-
+    private static final String m_tableKey = "TunableNumbers";
+    
     private String m_key;
     private boolean m_hasDefault = false;
     private double m_defaultVal;
-    private NetworkTableEntry nte_dashNumEntry;
-    private NetworkTableInstance nte_inst = NetworkTableInstance.getDefault(); //idk?
-    private HashMap<Integer, Double> m_lastHasChangedVals = new HashMap<>();
+    private double m_value;
 
-    public LoggedTunableNumber(String dashboardKey) {
-        m_key = m_tableKey + "/" + dashboardKey;
+    public LoggedTunableNumber(String key) {
+        this(key, 0.0);
     }
 
-    public LoggedTunableNumber(String dashboardKey, double defaultVal) {
-        this(dashboardKey);
+    public LoggedTunableNumber(String key, double defaultVal) {
+        m_key = m_tableKey + "/" + key;
         setDefault(defaultVal);
+        m_value = m_defaultVal;
     }
 
     public void setDefault(double defaultVal) {
-        if(!m_hasDefault) {
+        if (!m_hasDefault) {
             m_hasDefault = true;
             m_defaultVal = defaultVal;
-            if(RobotK.kTestMode) {
-                nte_dashNumEntry = new NetworkTableEntry(nte_inst, 1); // idk what a handle is T^T
-                nte_dashNumEntry.setDouble(defaultVal);
-            }
+        }
+        if (!DriverStation.isFMSAttached()) {
+            SmartDashboard.putNumber(m_key, SmartDashboard.getNumber(m_key, m_defaultVal));
         }
     }
 
     public double get() {
-        if(!m_hasDefault) {
+        if (!m_hasDefault) {
             return 0.0;
-        } 
-        return nte_dashNumEntry.getDouble(m_defaultVal);
-    }
-
-    public boolean hasChanged(int id) {
-        double currentVal = get();
-        double lastVal = m_lastHasChangedVals.get(id);
-
-        if(lastVal == 0 || currentVal != lastVal) {
-            m_lastHasChangedVals.put(id, currentVal);
-            return true;
         }
-
-        return false;
+        if (!DriverStation.isFMSAttached()) {
+            return m_value;
+        }
+        return m_defaultVal;
     }
 
-    public static void ifChanged(
-        int id, Consumer<double[]> action, LoggedTunableNumber... tunableNumbers) {
-            if (Arrays.stream(tunableNumbers).anyMatch(tunableNumber -> tunableNumber.hasChanged(id))) {
-                action.accept(Arrays.stream(tunableNumbers).mapToDouble(LoggedTunableNumber::get).toArray());
-            }
-    }
-
-    public static void ifChanged(int id, Runnable action, LoggedTunableNumber... tunableNumbers) {
-        ifChanged(id, values -> action.run(), tunableNumbers);
+    public void periodic() {
+        m_value = SmartDashboard.getNumber(m_key, m_defaultVal);
     }
 }
